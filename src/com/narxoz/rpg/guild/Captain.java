2dac0090048dad2d579ee62ae3ad8dc7ea28c1e0
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-05-11 21:59:09 +0500

    Implement Captain mediator communication and message handling

--- src/com/narxoz/rpg/guild/Captain.java
+++ src/com/narxoz/rpg/guild/Captain.java
@@ -17,5 +17,11 @@ public class Captain extends GuildMember {
     @Override
     public void receive(String topic, GuildMember from, String payload) {
         // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Captain " + getName() + "] received via " + topic
+                + " from " + from.getName()
+                + ": " + payload);
+        if ("COUNCIL".equals(topic)) {
+            System.out.println("[Captain " + getName() + "] acknowledges council update.");
+        }
     }
 }
