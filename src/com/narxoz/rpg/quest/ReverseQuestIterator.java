170171609f06fd4fae138e0708e1704be9b611e4
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-05-11 22:07:02 +0500

    Implement ReverseQuestIterator traversal logic

--- src/com/narxoz/rpg/quest/ReverseQuestIterator.java
+++ src/com/narxoz/rpg/quest/ReverseQuestIterator.java
@@ -18,12 +18,12 @@ public class ReverseQuestIterator implements QuestIterator {
     @Override
     public boolean hasNext() {
         // TODO: return true while the cursor still points at an unread quest.
-        return false;
+        return cursor < snapshot.size();
     }
 
     @Override
     public Quest next() {
         // TODO: return the current quest and move the cursor backward.
-        return null;
+        return snapshot.get(cursor++);
     }
 }
