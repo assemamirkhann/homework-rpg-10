cf5a4e4fdb7e871dd11bd5c9221c31f96d616bb6
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-05-11 22:02:40 +0500

    Implement Quartermaster mediator behavior and message handling

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -17,5 +17,13 @@ public class Quartermaster extends GuildMember {
     @Override
     public void receive(String topic, GuildMember from, String payload) {
         // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Quartermaster " + getName() + "] received via " + topic
+                + " from " + from.getName()
+                + ": " + payload);
+        if ("COUNCIL".equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] preparing supplies for approved missions.");
+        } else if ("SUPPLIES".equals(topic)) {
+            System.out.println("[Quartermaster " + getName() + "] processing supply request.");
+        }
     }
 }
