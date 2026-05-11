e0390fad91be009c9d816ff73f53992492995ef2
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-05-11 22:03:50 +0500

    Implement Scout mediator behavior and message handling logic

--- src/com/narxoz/rpg/guild/Scout.java
+++ src/com/narxoz/rpg/guild/Scout.java
@@ -16,6 +16,14 @@ public class Scout extends GuildMember {
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        // TODO: react to a guild-hall message without calling another colleague directly
+        System.out.println("[Scout " + getName() + "] received via " + topic
+                + " from " + from.getName()
+                + ": " + payload);
+        if ("COUNCIL".equals(topic)) {
+            System.out.println("[Scout " + getName() + "] analyzing routes for upcoming missions.");
+        } else if ("RECON".equals(topic)) {
+            System.out.println("[Scout " + getName() + "] updating map with new reconnaissance data.");
+        }
     }
 }
