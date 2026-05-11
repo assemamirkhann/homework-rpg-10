553543415c19e1f6e7c47e4126030fd5295b2d62
assemamirkhann <151700427+assemamirkhann@users.noreply.github.com>
2026-05-11 21:45:22 +0500

    Implement CouncilEngine using Iterator and Mediator patterns

--- src/com/narxoz/rpg/council/CouncilEngine.java
+++ src/com/narxoz/rpg/council/CouncilEngine.java
@@ -4,6 +4,8 @@ import com.narxoz.rpg.combatant.Hero;
 import com.narxoz.rpg.guild.GuildMediator;
 import com.narxoz.rpg.quest.QuestLog;
 import java.util.List;
+import com.narxoz.rpg.quest.QuestIterator;
+import com.narxoz.rpg.quest.Quest;
 
 /**
  * Orchestrates a planning session that uses both Iterator and Mediator.
@@ -14,6 +16,28 @@ public class CouncilEngine {
         // TODO: walk questLog with at least 2 different iterators,
         //       dispatch coordinating messages through hall for each quest,
         //       and return counters (questsTraversed, messagesRouted, membersNotified).
-        return new CouncilRunResult(0, 0, 0);
+        int questsTraversed = 0;
+        int messagesRouted = 0;
+        int membersNotified = 0;
+
+        QuestIterator forward = questLog.ordered();
+        while (forward.hasNext()) {
+            Quest quest = forward.next();
+            questsTraversed++;
+
+            hall.dispatch("COUNCIL", party.get(0), "Council reviewing quest: " + quest.getTitle());
+            messagesRouted += party.size();
+            membersNotified += party.size();
+        }
+        QuestIterator reverse = questLog.reverse();
+        while (reverse.hasNext()) {
+            Quest quest = reverse.next();
+            questsTraversed++;
+
+            hall.dispatch("COUNCIL", party.get(0), "Re-evaluating quest: " + quest.getTitle());
+            messagesRouted += party.size();
+            membersNotified += party.size();
+        }
+        return new CouncilRunResult(questsTraversed, messagesRouted, membersNotified);
     }
 }
